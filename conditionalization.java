c82537aa51ea77599540bf142b896f0bddfd0145
wafaaidan <wafa.aidan@gmail.com>
2025-10-28 15:00:54 +0700

    Membuat suatu kondisi Statement

---
+++ conditionalization.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class conditionalization {
+    public static void main(String[] args) {
+        Scanner ns = new Scanner(System.in);
+
+        //int nilai;
+        
+
+        //System.out.println("Masukkan nilai= ");
+        //nilai = ns.nextInt();
+
+        //if (nilai != 90) {
+        //    System.out.println("Lulus");
+            
+        //} else {
+        //    System.out.println("tidak lulus");
+        //}
+
+        int nilaiMTK;
+        int nilaiInggris;
+        int nilaiFisika;
+
+        System.out.println("Mauskkan nilai MTK= ");
+        nilaiMTK = ns.nextInt();
+
+        System.out.println("Mauskkan nilai Inggris= ");
+        nilaiInggris = ns.nextInt();
+
+        System.out.println("Mauskkan nilai Fisika= ");
+        nilaiFisika = ns.nextInt();
+
+        if (nilaiMTK == 75 && nilaiInggris != 75) {
+            System.out.println("Lulus");
+        } else {
+            System.out.println("Tidak lulus lulus");
+        }
+
+        if (nilaiInggris > 90 && nilaiFisika < 75) {
+            System.out.println("Cerdas");
+        } else {
+            System.out.println("KURANG belAJAR");
+        }
+        ns.close();
+    }
+}
